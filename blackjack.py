24b9706b84f4853bdff324fa134418d583166319
ankylosorus <91287515+ankylosorus@users.noreply.github.com>
2022-06-25 21:01:51 +0200

    add actions options

--- blackjack.py
+++ blackjack.py
@@ -6,8 +6,7 @@ import getpass
 
 import time
 
-deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace", "Ace", "Ace", "Ace", "Ace", "Ace", "Ace"]
-#deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 
 deck52 = deck *4
 
@@ -146,22 +145,27 @@ if "Ace" in playerhand:
             newchoice = int(input("\n    ---> is second Ace worth 1 or 11?\n"))
             acechoice.append(newchoice)
         print(acechoice)
-            
-print(f"       => total: {total(playerhand)}")
+ 
+time.sleep(1)
 
 if total(playerhand) == 21:
-    print("     BLACKJACK!\n")
+    print("     BLACKJACK!")      
+else:           
+    print(f"       => total: {total(playerhand)}")
+
 
 #****************************************
-#***********DEMANDE DE CARTES************
+#*************CHOIX D'ACTION*************
 #****************************************
 time.sleep(1)
 
 if total(playerhand) < 21:
     
-    hit = input("\nSay 'hit' if you want another card.\nIf you don't the dealer hand will be revealed.\n")
+    action = input("\nWhat do you wish to do now?\nSay 'hit' if you want another card.\nSay 'stand' if you want the hand to end. The dealer hand will be revealed.\nSay 'double' if you want to double your bet. You will draw one last card.\n")
+    #hit = input("\nSay 'hit' if you want another card.\nIf you don't the dealer hand will be revealed.\n")
         
-    while hit.lower() == "hit" and total(playerhand) < 21:
+    #while hit.lower() == "hit" and total(playerhand) < 21:
+    while action.lower() == "hit":
         card = random.choice(deck52)
         print("\nHere's your card:")
         time.sleep(1)
@@ -174,13 +178,32 @@ if total(playerhand) < 21:
                 acechoice.append(newchoice)
         print(f"      => total: {total(playerhand)}")
         time.sleep(1)
-        if total(playerhand) == 21:
-            print("     BLACKJACK!\n")
-        elif total(playerhand) > 21:
-            print("     BUST! You lose, you're over 21.\n")
+        #si blackjack ou supérieur à 21: pas de choix à faire
+        if total(playerhand) >= 21:
+            break
         else:
-            hit = input("\nAgain? Say 'hit'\n")
-
+            #hit = input("\nAgain? Say 'hit'\n")
+            action = input("\nWhat now? Say 'hit', 'double' or 'stand'.\n")
+            
+    if action == "double":
+        bet = bet*2
+        card = random.choice(deck52)
+        print("\nDOUBLE DOWN! Here's your card:")
+        time.sleep(1)
+        drawcard(playerhand)
+        print(styled(playerhand))
+        if playerhand[-1] == "Ace":
+            newchoice = 0
+            while newchoice != 11 and newchoice != 1:
+                newchoice = int(input("\n---> is Ace worth 1 or 11?\n"))
+                acechoice.append(newchoice)
+        print(f"      => total: {total(playerhand)}")
+        time.sleep(1)
+    
+    if action == "stand":
+        time.sleep(1)
+        print("You will take no more card")
+    
 #***************************************************
 #******REVEAL ET COMPLETE DE LA MAIN DU DEALER******
 #***************************************************
@@ -188,43 +211,53 @@ time.sleep(2)
 
 print("\nThe hand ends. Revealing dealer's hand...\n")
 
-time.sleep(2)
+time.sleep(1)
 
-print(f"Dealer:\n{styled(dealerhand)}")
-
-#print(f"       => total: {total(dealerhand)}\n")
+print(f"Dealer:\n{styled(dealerhand)}\n")
 
 if total(dealerhand) < 17:
     time.sleep(1)
-    print("Completing dealer's hand until 17...\n")    
+    print("Completing dealer's hand until 17...")
+    time.sleep(2)
 while total(dealerhand) < 17:
     time.sleep(1)
-    print("...\n")
     drawcard(dealerhand)
     print(styled(dealerhand))
-print(f"       => total: {total(dealerhand)}")
-if total(dealerhand) > 21:
-    print("Dealer loses")
+print(f"       => total: {total(dealerhand)}\n")
+# if total(dealerhand) > 21:
+#     print("Dealer loses")
 
 #***************************************************
-#*********************GAINS*************************
-#*************************************************** 
+#****************GAINS DU JOUEUR********************
+#*****************hors blackjack********************
+#***************************************************
+
+#***perdu***
+if total(playerhand) > 21:
+    print("Your bet is lost")        
+
+#***soft blackjack et tie blackjack***
 if total(playerhand) == 21 and total(dealerhand) != 21:
     time.sleep(1)
-    print("BLACKJACK! You win")
-    wallet = wallet + bet + 1.5*bet
+    print("You win once your bet.\n")
+    wallet = wallet + 2*bet
 
 if total(dealerhand) == total(playerhand) == 21:
     time.sleep(1)
-    print("PUSH! Your bet is returned to you")
+    print("TIE! Your bet is returned to you.\n")
     wallet = wallet + bet
     
-if total(dealerhand) > total(playerhand):
+#***moins de 21***
+if total(playerhand) < 21:
     time.sleep(1)
-    print("Dealer wins")
-    
-if total(playerhand) > total(dealerhand):
-    time.sleep(1)
-    print("Player wins")
+    if total(dealerhand) <= 21:
+        if total(dealerhand) > total(playerhand):
+            print("Dealer wins. Your bet is lost.\n")
+        elif total(dealerhand) == total(playerhand):
+            print("PUSH! Your bet is returned to you.\n")
+        else:
+            print("Your hand's better than the dealer's. You win once your bet.\n")
+            wallet = wallet + bet*2
 
-print(f"Wallet: {wallet}")
+time.sleep(1)             
+print(f"You have {wallet}$ left.")
